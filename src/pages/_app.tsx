d71ea9319267c6da9e9762ba7b4581716511de3d
techandmedia <eko.andri@icloud.com>
2022-04-08 14:27:53 +0700

    error Cannot read properties of undefined (reading 'send')

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,33 @@
+import { useEffect } from "react";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 import awsExports from "../../aws-exports";
 
 Amplify.configure({ ...awsExports, ssr: true });
 
 export default function MyApp({ Component, pageProps }) {
+  const { user, signOut } = useAuthenticator((context) => [context.user]);
+
+  useEffect(() => {
+    console.log("App", user);
+    console.log("App", signOut);
+  }, [user, signOut]);
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
-        </main>
-      )}
-    </Authenticator>
+    <main>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+      <Component {...pageProps} />
+    </main>
+    // <Authenticator>
+    //   {({ signOut, user }) => (
+    //     <main>
+    //       <h1>Hello {user.username}</h1>
+    //       <button onClick={signOut}>Sign out</button>
+    //       <Component {...pageProps} />
+    //     </main>
+    //   )}
+    // </Authenticator>
   );
 }
