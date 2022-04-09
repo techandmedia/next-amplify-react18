9e5b59f5f85c086f6b9c90b937d8675619dded57
techandmedia <eko.andri@icloud.com>
2022-04-09 11:41:04 +0700

    fix with adding a wrapper provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,21 +6,29 @@ import awsExports from "../../aws-exports";
 
 Amplify.configure({ ...awsExports, ssr: true });
 
-export default function MyApp({ Component, pageProps }) {
+export default function AppWrapper(props) {
+  return (
+    <Authenticator.Provider>
+      <MyApp {...props} />
+    </Authenticator.Provider>
+  );
+}
+
+function MyApp({ Component, pageProps }) {
   const { user, signOut } = useAuthenticator((context) => [context.user]);
 
   useEffect(() => {
-    console.log("App", user);
-    console.log("App", signOut);
+    console.log("user", user);
+    console.log("signOut", signOut);
   }, [user, signOut]);
 
   return (
     <main>
-      <h1>Hello {user.username}</h1>
+      <h1>Hello {user?.username || "Guest"}</h1>
       <button onClick={signOut}>Sign out</button>
       <Component {...pageProps} />
     </main>
-    // <Authenticator>
+    // <Authenticator.Provider>
     //   {({ signOut, user }) => (
     //     <main>
     //       <h1>Hello {user.username}</h1>
@@ -28,6 +36,6 @@ export default function MyApp({ Component, pageProps }) {
     //       <Component {...pageProps} />
     //     </main>
     //   )}
-    // </Authenticator>
+    //   <Authenticator.Provider>
   );
 }
